7979211011c10a0457f067648fd2604800738708
Soulistic <smfaizalam786@gmail.com>
2024-03-09 00:05:37 +0530

    Landing Page

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,24 @@
+import Navbar from '@/components/Navbar';
+import React from 'react';
+
+type indexProps = {
+    
+};
+
+const index:React.FC<indexProps> = () => {
+    React.useEffect(() => {
+        // Disable scrolling when the component mounts
+        document.body.style.overflow = 'hidden';
+        // Re-enable scrolling when the component unmounts
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, []);
+    return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+        <Navbar></Navbar>
+        <div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none '>
+            <img src='/hero.png' alt='hero image'/>
+        </div>
+    </div>
+}
+export default index;

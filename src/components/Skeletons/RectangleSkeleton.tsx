5de6b1d8b4db9f220e762343d1886de1b23d097d
Soulistic <smfaizalam786@gmail.com>
2024-05-19 14:50:09 +0530

    added loading skeletons of likes,dislikes

---
+++ src/components/Skeletons/RectangleSkeleton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type RectangleSkeletonProps = {
+    
+};
+
+const RectangleSkeleton:React.FC<RectangleSkeletonProps> = () => {
+    
+    return (
+        <div className='space-y-2.5 animate-pulse'>
+            <div className='flex items-center w-full'>
+                <div className='h-6 w-12 bg-dark-fill-3 rounded-full'></div>
+            </div>
+
+        </div>
+    )
+}
+export default RectangleSkeleton;
--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -5,6 +5,8 @@ import { BsCheck2Circle } from "react-icons/bs";
 import { TiStarOutline } from "react-icons/ti";
 import { doc, getDoc } from "firebase/firestore";
 import { firestore } from "@/firebase/firebase";
+import RectangleSkeleton from "@/components/Skeletons/RectangleSkeleton";
+import CircleSkeleton from "@/components/Skeletons/CircleSkeleton";
 
 type ProblemDescriptionProps = {
     problem: Problem;
@@ -51,6 +53,15 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({problem}) => {
 								<div className='cursor-pointer hover:bg-dark-fill-3  rounded p-[3px]  ml-4 text-xl transition-colors duration-200 text-green-s text-dark-gray-6 '>
 									<TiStarOutline />
 								</div>
+							</div>
+						)}
+						{loading &&(
+							<div className="mt-3 flex space-x-2">
+								<RectangleSkeleton></RectangleSkeleton>
+								<CircleSkeleton></CircleSkeleton>
+								<RectangleSkeleton></RectangleSkeleton>
+								<RectangleSkeleton></RectangleSkeleton>
+								<CircleSkeleton></CircleSkeleton>
 							</div>
 						)}
 
